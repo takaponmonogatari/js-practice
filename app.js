f330ca19c4874adba0eca3ee4610e7a81c184301
takaponmonogatari <taka11hiro26@gmail.com>
2021-05-08 15:36:48 +0900

    [Finish] 6-6

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-$(function(){
-  $(".box1").slideDown(function (){
-    $(".box1").css({
-      "background-color": "#0000FF",
-      "width": "200px",
-      "hight": "100px"
-    }).slideUp(3000);
+$(function (){
+  $(".box1").on("click", function(){
+    $(".box1").addClass("box1-ext");
+  })
+  $(".box1").mouseout(function(){
+    $(".box1").removeClass("box1-ext");
   })
 });
